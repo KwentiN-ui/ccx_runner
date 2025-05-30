67c61bb19821ea2f1123053243e87173025526c5
KwentiN <quentinhuss@hotmail.com>
2025-05-30 19:46:40 +0200

    reading iteration data

--- ccx_runner/ccx_logic/ccx_status.py
+++ ccx_runner/ccx_logic/ccx_status.py
@@ -13,6 +13,7 @@ class CalculixStatus:
         self.hauptfenster = hauptfenster
         self.running = False
         self.steps: list[Step] = []
+        self.parsed_lines: list[str] = []
 
     def parse(self, line: str):
         line = line.strip()
@@ -50,5 +51,18 @@ class CalculixStatus:
             self.steps[-1].increments[-1].iterations.append(
                 Iteration(self.steps[-1].increments[-1], number)
             )
+        
+        # look for Iteration Residuals
+        if ("convergence" in line) or ("no convergence" in line):
+            iteration = self.steps[-1].increments[-1].iterations[-1]
 
+            for prev_line in self.parsed_lines[-2::-1]:
+                # gehe Rückwärts bis du eine leere Zeile findest
+                if len(prev_line.strip()) == 0: # leere Zeile
+                    break # out of for loop
+                else:
+                    name,_,wert = prev_line.partition("=")
+                    iteration.data[name] = float(wert.strip().partition(" ")[0])
+        
+        self.parsed_lines.append(line)
         self.hauptfenster.update_solver_status()
--- ccx_runner/ccx_logic/iteration.py
+++ ccx_runner/ccx_logic/iteration.py
@@ -1,8 +1,11 @@
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from ccx_runner.ccx_logic.increment import Increment
 
+
 class Iteration:
-    def __init__(self, increment:"Increment", number:int) -> None:
+    def __init__(self, increment: "Increment", number: int) -> None:
         self.increment = increment
         self.number = number
+        self.data: dict[str, float] = {}
